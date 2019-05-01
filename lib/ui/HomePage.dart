49f74acfb133a03b1aa9881ca217d25c0bc7ef64
Gustavo Moeska <32044172+DarkoBlaze@users.noreply.github.com>
2019-05-01 18:56:45 -0300

    New structure

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -2,223 +2,130 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HomeState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Tela de login",
-          style: TextStyle(
-            fontSize: 17.0,
-          ),
+
+    final _image = SizedBox(
+      height: 150.0,
+      width: 150.0,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(100.0),
         ),
-        centerTitle: true,
-        backgroundColor: Color.fromRGBO(65, 10, 75, 100),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.refresh),
-            color: Colors.white,
+      ),
+    );
+
+    final _loginText = TextField(
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: "Username",
+        hintStyle: TextStyle(
+          color: Color.fromRGBO(165, 165, 215, 100),
+        ),
+        icon: Icon(Icons.person, color: Color.fromRGBO(165, 165, 205, 100)),
+        contentPadding: EdgeInsets.only(top: 20.0, left: 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.horizontal()),
+      ),
+    );
+
+    final _passwordText = TextField(
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: "Password",
+        hintStyle: TextStyle(
+          color: Color.fromRGBO(165, 165, 215, 100),
+        ),
+        icon: Icon(Icons.lock, color: Color.fromRGBO(165, 165, 205, 100)),
+        contentPadding: EdgeInsets.only(top: 20.0, left: 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.horizontal()),
+      ),
+    );
+
+    final _buttonLogin = RaisedButton(
+      onPressed: () {},
+      child: Text(
+        "Login",
+        style: TextStyle(
+          fontSize: 15.0,
+          color: Color.fromRGBO(165, 165, 215, 100),
+        ),
+      ),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+      ),
+      padding: EdgeInsets.all(12),
+      elevation: 4.0,
+      color: Color.fromRGBO(105, 105, 135, 300),
+    );
+
+    final _buttonNewAccount = FlatButton(
+      onPressed: () {},
+      child: Text(
+        "Nova conta",
+        style: TextStyle(
+          color: Color.fromRGBO(110, 110, 205, 100),
+          fontSize: 16.0,
+        ),
+      ),
+    );
+
+    final _buttonForgotPassword = FlatButton(
+      onPressed: () {},
+      child: Text(
+        "Esqueceu senha?",
+        style: TextStyle(
+          color: Color.fromRGBO(110, 110, 205, 100),
+          fontSize: 16.0,
+        ),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(65, 65, 105, 100),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 110.0, bottom: 35.0),
+            child: _image,
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 50.0, top: 30.0, bottom: 15.0),
+            child: _loginText,
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 50.0),
+            child: _passwordText,
+          ),
+
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 50.0, right: 30.0, top: 30.0),
+                child: _buttonNewAccount,
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 10.0, top: 30.0),
+                child: _buttonForgotPassword,
+              ),
+            ],
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(30.0),
+            child: SizedBox(
+              width: 300.0,
+              child: _buttonLogin,
+            ),
           ),
         ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            // Menu
-            Padding(
-              padding: EdgeInsets.only(bottom: 0.4),
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(65, 10, 75, 100),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 132.0, top: 40.0),
-                    child: Text(
-                      "Menu",
-                      style: TextStyle(
-                        color: Color.fromRGBO(45, 5, 55, 65),
-                        fontSize: 18.0,
-                      ),
-                    ),
-                  )
-                ),
-              ),
-            ),
-
-            // Buttons
-            SizedBox(
-              width: 100,
-              height: 40,
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(45, 5, 55, 65),
-                ),
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      color: Colors.white70,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            SizedBox(
-              width: 100,
-              height: 40,
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(45, 5, 55, 65),
-                ),
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "New Account",
-                    style: TextStyle(
-                      color: Colors.white70,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            SizedBox(
-              width: 100,
-              height: 40,
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(45, 5, 55, 65),
-                ),
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Help",
-                    style: TextStyle(
-                      color: Colors.white70,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            SizedBox(
-              width: 100,
-              height: 40,
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(45, 5, 55, 65),
-                ),
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Exit",
-                    style: TextStyle(
-                      color: Colors.white70,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 40.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 10.0, top: 10.0),
-              child: SizedBox(
-                width: 150.0,
-                height: 150.0,
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(65, 65, 65, 100),
-                    borderRadius: BorderRadius.circular(100.0),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 65.0, left: 42.0),
-                    child: Text(
-                      "Imagem",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Login: ",
-                labelStyle: TextStyle(
-                  color: Color.fromRGBO(65, 10, 75, 100),
-                ),
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Senha: ",
-                labelStyle: TextStyle(
-                  color: Color.fromRGBO(65, 10, 75, 100),
-                ),
-              ),
-            ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-                  child: FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Esqueceu senha?",
-                      style: TextStyle(
-                      color: Color.fromRGBO(65, 10, 75, 100),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0, left: 40.0, right: 20.0),
-                  child: FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Nova conta",
-                      style: TextStyle(
-                      color: Color.fromRGBO(65, 10, 75, 100),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 15.0),
-              child: RaisedButton(
-                onPressed: () {},
-                color: Color.fromRGBO(65, 10, 75, 100),
-                child: Text(
-                  "Entrar",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
